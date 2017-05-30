8ff6e6bf7512c3ac876d7549b2c0f6e922025144
remi-san <remi.san@gmail.com>
2017-05-30 12:20:11 +0200

    feat: show errors

--- app/controllers.php
+++ app/controllers.php
@@ -3,8 +3,14 @@
 use ControllerProvider\PokemonControllerProvider;
 use ServiceProvider\ControllerServiceProvider;
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /** @var Application $app */
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    return new Response(nl2br($e->getMessage() . PHP_EOL . $e->getTraceAsString()), $code);
+});
+
 $app->register(new ControllerServiceProvider());
 
 // Pokemon
--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
+
 ini_set('display_errors', 0);
 
 require_once __DIR__.'/../vendor/autoload.php';
+
+Debug::enable();
 
 $app = require __DIR__.'/../app/app.php';
 require __DIR__.'/../config/prod.php';
